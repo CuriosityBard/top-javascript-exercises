a82bf564f7fa7b4e078f6b559397e32921e1dd94
CuriosityBard <curiositybard@gmail.com>
2022-06-21 08:42:27 -0700

    Complete sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return 'ERROR';
+    }
+    if (a < 0 || b < 0) {
+        return 'ERROR';
+    }
 
+    let bigNum = 0;
+    let smallNum = 0;
+
+    if (a > b) {
+        bigNum = a;
+        smallNum = b;
+    } else if (b > a) {
+        bigNum = b;
+        smallNum = a;
+    } else {
+        return a;
+    }
+
+    let total = 0;
+
+    let numOfNums = bigNum - smallNum;
+    for (let i = 0; i < numOfNums; i++) {
+        total += smallNum;
+        smallNum++;
+    }
+    total += bigNum;
+
+    return total;
 };
 
 // Do not edit below this line
