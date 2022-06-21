2338df4545979dc5505bff821a1fff7a0b3f5255
CuriosityBard <curiositybard@gmail.com>
2022-06-21 07:39:18 -0700

    Complete reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(reverseThisString) {
+    let arrayOfChars = reverseThisString.split("");
+    let returnArray = [];
 
+    for (let i = arrayOfChars.length - 1; i >= 0; i--) {
+        returnArray.push(arrayOfChars[i]);
+    }
+
+    let returnString = returnArray.join("");
+    return returnString;
 };
 
 // Do not edit below this line
