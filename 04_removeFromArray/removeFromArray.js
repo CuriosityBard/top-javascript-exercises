b98d323c9a72e6703dbb36880e54f99355554d9e
CuriosityBard <curiositybard@gmail.com>
2022-06-21 08:01:56 -0700

    Complete removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayOfValues, ...args) {
+    let valuesToRemove =  Array.from(args);
 
+    let returnArray = [...arrayOfValues];
+
+    for (let value of valuesToRemove) {
+        if (returnArray.includes(value)) {
+            let pos = returnArray.indexOf(value);
+            returnArray = [].concat(returnArray.slice(0, pos), returnArray.slice(pos+1, returnArray.length));
+        } 
+    }
+
+    return returnArray;
 };
+
+removeFromArray([1], 5, 3, 6, 76, 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
