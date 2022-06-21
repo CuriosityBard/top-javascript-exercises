65482f9848f27ad145adf417d6fe70f4b3b4fd53
CuriosityBard <curiositybard@gmail.com>
2022-06-21 08:54:40 -0700

    Complete tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let celcius = (temp - 32) * (5/9);
+  celcius = Math.round(celcius * 10)/10;
+  return celcius;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let farenheit = (temp * (9/5)) + 32;
+  farenheit = Math.round(farenheit * 10)/10;
+  return farenheit;
 };
 
 // Do not edit below this line
