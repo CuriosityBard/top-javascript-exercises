3ae016ff8f83f0881465196ed6cdd33b24de4837
CuriosityBard <curiositybard@gmail.com>
2022-06-21 07:31:29 -0700

    Complete repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(strToRepeat, repeatAmount) {
+    let returnString = "";
 
+    // negative numbers won't work with for loop
+    if (repeatAmount < 0) {
+        return 'ERROR';
+    }
+
+    for (let i = 0; i < repeatAmount; i++) {
+        returnString += strToRepeat;
+    }
+    return returnString;
 };
 
 // Do not edit below this line
