85a99c14b4ccf223d14d869e3645deb1ea743ee0
CuriosityBard <curiositybard@gmail.com>
2022-06-28 09:39:33 -0700

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	let total = 0;
+  for (arg of a) {
+    total += arg;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  total = 1;
+  for (arg of a) {
+    total *= arg;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let total = 1;
+  for (let i = 2; i <= a; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
