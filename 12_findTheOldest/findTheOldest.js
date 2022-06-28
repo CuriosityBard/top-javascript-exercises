0be54002ee7d217861583186457803d5e03f0ff5
CuriosityBard <curiositybard@gmail.com>
2022-06-28 10:47:31 -0700

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    function whichOlder(person1Death, person1Birth, person2Death, person2Birth) {
+        if (person1Death - person1Birth > person2Death - person2Birth) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
+    return people.reduce((prevPerson, person) => {
+        let prevPersonDeath = prevPerson.yearOfDeath;
+        let personDeath = person.yearOfDeath;
+
+        if (!prevPersonDeath) {
+            prevPersonDeath = (new Date()).getFullYear();
+        }
+        if (!personDeath) {
+            personDeath = (new Date()).getFullYear();
+        }
+
+        if (whichOlder(personDeath, person.yearOfBirth, prevPersonDeath, prevPerson.yearOfBirth)) {
+            console.log(person);
+            return person;
+        } else {
+            console.log(prevPerson);
+            return prevPerson;
+        }
+    });
 };
 
 // Do not edit below this line
