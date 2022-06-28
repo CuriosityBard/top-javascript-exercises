b66f9289847a52e2323714f87a68bba5310cdd65
CuriosityBard <curiositybard@gmail.com>
2022-06-28 10:19:48 -0700

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 1) {
+        return 'OOPS';
+    }
+    let returnThis = 1;
+    let addThis = 0;
+    for (let i = 0; i < n-1; i++) {
+        let prev = returnThis;
+        returnThis += addThis;
+        addThis = prev;
+    }
+    return returnThis;
 };
 
 // Do not edit below this line
